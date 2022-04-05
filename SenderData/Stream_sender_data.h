45f50cac8a6c1b33c0fdc795678db66725a840bb
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-05 23:19:34 +0530

    Update Stream_sender_data.h

--- SenderData/Stream_sender_data.h
+++ SenderData/Stream_sender_data.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define MAX_SENSOR_READINGS 100  
+#define MAX_SENSOR_READINGS 50  
 
 typedef enum {
   OK,
@@ -17,5 +17,6 @@ typedef enum {
 extern int buffSize;
 extern FileAccess getSensorData();
 extern FileAccess displayReadingsOnConsole(DataStreamMode startTrasmissionReq);
+extern int ReadingCount[MAX_SENSOR_READINGS]; 
 extern int BatterySoC[MAX_SENSOR_READINGS]; //buffer to store BMS SoC value
 extern float BatteryVoltage[MAX_SENSOR_READINGS];  //buffer to store BMS Voltage value
