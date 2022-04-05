7a6ecb3426c33cadee6daf3ff98873656bc20f8d
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-05 20:13:54 +0530

    Update test_cases.cpp

--- SenderData/test_cases.cpp
+++ SenderData/test_cases.cpp
@@ -11,7 +11,7 @@ TEST_CASE("To check if file is successfully opened for read data access")
 TEST_CASE("To check for unsuccessful attempt of opening the input file for read data access") 
 { 
  FILE *sensData_fp=NULL;
- REQUIRE(!getSensorData()== NOK); 
+ REQUIRE(getSensorData()== NOK); 
 }
 
 TEST_CASE("To check the data read from the file is displayed on the console") 
