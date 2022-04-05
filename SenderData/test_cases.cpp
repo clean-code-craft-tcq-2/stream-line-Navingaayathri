efa902f9182ecfb9de7aec772462d8bccb10ca36
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-05 17:29:49 +0530

    Add files via upload
    Added test cases

---
+++ SenderData/test_cases.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test-framework/catch.hpp"
+#include "Stream_sender_data.h"
+
+TEST_CASE("To check if file is successfully opened for read data access") 
+{ 
+ REQUIRE(getSensorData()== OK); 
+}
+
+TEST_CASE("To check the data read from the file is displayed on the console") 
+{
+  DataStreamMode startTrasmissionReq=Transmitdata;
+  REQUIRE(displayReadingsOnConsole(startTrasmissionReq)== OK)
+}
