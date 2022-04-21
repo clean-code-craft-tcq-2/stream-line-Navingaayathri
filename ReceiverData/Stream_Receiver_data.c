e94b0d366c90fcbc2b449713c749f9d8b9a75fa6
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 17:05:02 +0530

    Update Stream_Receiver_data.c

--- ReceiverData/Stream_Receiver_data.c
+++ ReceiverData/Stream_Receiver_data.c
@@ -13,15 +13,10 @@ ReadDataFromConsole (int *SoCRead, float *VoltageRead, int Size)
 {
   char ReadString[500];
   char *pt;
- // printf ("\n ReadDtatfromConsole");
-//  scanf ("%[^\n],%50s", ReadString);	//File opened successfully
-//  scanf ("%50s", ReadString);	//ReadingsCount,BatterySoC
-//  scanf ("%[^\n]", ReadString);	//in percentage,BatteryVoltage in volts
   for (int i = 0; i <Size ; i++)
     {
       scanf ("%[^,]", ReadString);	//1
       scanf ("%50s", ReadString);
-//      printf ("\nReading1 %s\n", ReadString);
         CommaSeparatedData(ReadString,i);
     }
 }
@@ -29,16 +24,11 @@ void CommaSeparatedData(char str[], int index)
 {
         char *pt;
         pt = strtok (str, ",");
-     //   int a = atoi(pt);
-    //    pt = strtok (str, ",");
-  //    while (pt != NULL)
-	{
 	  BatterySoCRead[index] = atoi (pt);
 //	  printf ("\nBatterySoCRead[%d]: %d", index,BatterySoCRead[index]);
 	  pt = strtok (NULL, ",");
 	  BatteryVoltageRead[index]= atof(pt);
 //	  printf("\nBatteryVoltageRead[%d]: %f", index,BatteryVoltageRead[index]);
-	}
 }
 int
 DisplayReceivedData (int SampleToDisplay)
