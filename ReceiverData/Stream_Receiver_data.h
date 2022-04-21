68faddc6f9c687489c734bd6816512183e3fed95
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 15:38:43 +0530

    Update Stream_Receiver_data.h

--- ReceiverData/Stream_Receiver_data.h
+++ ReceiverData/Stream_Receiver_data.h
@@ -1,1 +1,11 @@
 /*Receiver.h*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void ReadDataFromConsole(int *ReadBatterySoC, float *ReadBatteryVoltage, int Size);
+extern int DisplayReceivedData();
+extern void CommaSeparatedData(char str[], int index);
+extern int BatterySoCRead[50];
+extern float BatteryVoltageRead[50];
