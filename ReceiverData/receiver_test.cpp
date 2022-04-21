5026a33f563d5f6caad7cd861d95a28c9cfd74e4
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 15:34:16 +0530

    Create receiver_test.cpp

---
+++ ReceiverData/receiver_test.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test-framework/catch.hpp"
+#include "Stream_Receiver_data.h"
+TEST_CASE("Test code")
+{
+
+}
