89e474fb59277c06f6823a8dbdf336bb3dd1f5b2
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 22:13:23 +0530

    Update Stream_Receiver_data.h

--- ReceiverData/Stream_Receiver_data.h
+++ ReceiverData/Stream_Receiver_data.h
@@ -9,5 +9,11 @@
 void ReadDataFromConsole(int *ReadBatterySoC, float *ReadBatteryVoltage, int Size);
 extern int DisplayReceivedData(int SampleToDisplay);
 extern void CommaSeparatedData(char str[], int index);
+extern int MaxSoc(int count);
+extern int MinSoc(int count);
+extern float MaxVoltage(int count);
+extern float MinVoltage(int count);
 extern int BatterySoCRead[50];
 extern float BatteryVoltageRead[50];
+extern int MaxSocValue,MinSocValue;
+extern float MaxVoltageValue,MinVoltageValue;
