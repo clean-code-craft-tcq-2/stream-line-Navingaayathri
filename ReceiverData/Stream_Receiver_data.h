4dfa5b5072d591a968dd712e44d3bd59d2d6cf3b
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 15:53:10 +0530

    Update Stream_Receiver_data.h

--- ReceiverData/Stream_Receiver_data.h
+++ ReceiverData/Stream_Receiver_data.h
@@ -7,7 +7,7 @@
 #define MAX_SENSOR_SAMPLES 50
 
 void ReadDataFromConsole(int *ReadBatterySoC, float *ReadBatteryVoltage, int Size);
-extern int DisplayReceivedData();
+extern int DisplayReceivedData(int SampleToDisplay);
 extern void CommaSeparatedData(char str[], int index);
 extern int BatterySoCRead[50];
 extern float BatteryVoltageRead[50];
