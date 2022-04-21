67f65bad7738ce1a87ae5541e9af8dbf5bb3a563
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 22:30:39 +0530

    Update receiver_test.cpp

--- ReceiverData/receiver_test.cpp
+++ ReceiverData/receiver_test.cpp
@@ -2,30 +2,31 @@
 
 #include "test-framework/catch.hpp"
 #include "Stream_Receiver_data.h"
-TEST_CASE("Test code")
+/*TEST_CASE("Test code")
 {
   int samples=50, value =0;
 value= DisplayReceivedData(samples);
-}
-
+}*/
+/*
 TEST_CASE("Test code Read 3 samples from Console")
 {
   int samples=3, value =0;
   printf("1,100,12.90\n");
   ReadDataFromConsole(BatterySoCRead,BatteryVoltageRead,samples);
   DisplayReceivedData(samples);
-}
-
+}*/
+/*
 TEST_CASE("Test code Parse")
 {
   char buff[20]="96,12.80";
   CommaSeparatedData(buff,1);
-}
+}*/
 
 TEST_CASE("Test code check for maximum and minimum")
 {
   int samples=3, value =0;
   ReadDataFromConsole(BatterySoCRead,BatteryVoltageRead,samples);
+  DisplayReceivedData(samples);
  MaxSocValue=MaxSoc(samples);
  MinSocValue=MinSoc(samples);
  MaxVoltageValue=MaxVoltage(samples);
