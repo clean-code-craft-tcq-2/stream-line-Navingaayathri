b20f8194c0643d7cdee7182bc18de348b3eab093
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 22:20:29 +0530

    Update receiver_test.cpp

--- ReceiverData/receiver_test.cpp
+++ ReceiverData/receiver_test.cpp
@@ -21,3 +21,19 @@ TEST_CASE("Test code Parse")
   char buff[20]="96,12.80";
   CommaSeparatedData(buff,1);
 }
+
+TEST_CASE("Test code check for maximum and minimum")
+{
+  int samples=3, value =0;
+  ReadDataFromConsole(BatterySoCRead,BatteryVoltageRead,samples);
+ MaxSocValue=MaxSoc(samples);
+ MinSocValue=MinSoc(samples);
+ MaxVoltageValue=MaxVoltage(samples);
+ MinVoltageValue=MinVoltage(samples);
+  printf("\nMaxSocValue:%d",MaxSocValue);
+printf("\nMinSocValue:%d",MinSocValue);
+printf("\nMaxVoltageValue:%f",MaxVoltageValue);
+printf("\nMinVOltageValue:%f",MinVoltageValue);
+ 
+ // REQUIRE(MaxSocValue==expectedMaxSocValue);
+}
