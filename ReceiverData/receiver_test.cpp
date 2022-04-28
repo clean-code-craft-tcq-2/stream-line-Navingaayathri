ba3dd60d26e155508dc83c9e89d31a63db62d39e
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-28 10:24:18 +0530

    Update receiver_test.cpp

--- ReceiverData/receiver_test.cpp
+++ ReceiverData/receiver_test.cpp
@@ -17,6 +17,7 @@ TEST_CASE("Test code Parse")
 TEST_CASE("Test code check for maximum and minimum")
 {
   int samples=3, value =0;
+  float *p;
   float expectedMaxVoltageValue=12.90,expectedMinVoltageValue=12.80;
   int expectedMaxSocValue=100,expectedMinSocValue=96;
   ReadDataFromConsole(BatterySoCRead,BatteryVoltageRead,samples);
@@ -25,10 +26,12 @@ TEST_CASE("Test code check for maximum and minimum")
  MinSocValue=MinSoc(samples);
  MaxVoltageValue=MaxVoltage(samples);
  MinVoltageValue=MinVoltage(samples);
+  p=MovingAverage(SMA,samples);
   printf("\nMaxSocValue:%d",MaxSocValue);
 printf("\nMinSocValue:%d",MinSocValue);
 printf("\nMaxVoltageValue:%f",MaxVoltageValue);
 printf("\nMinVOltageValue:%f",MinVoltageValue);
+  printf("\nSMA value SOC : %f, Voltage : %f",p[0],p[1]);
  
  REQUIRE(MaxSocValue==expectedMaxSocValue);
   REQUIRE(MinSocValue==expectedMinSocValue);
